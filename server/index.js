31a82d787894bfbb572e8c174bd8d3e38b6fd8e0
Ben Kidder <r.ben.kidder@gmail.com>
2020-02-24 12:40:57 -0800

    Refactor backend to support ArangoDB
    Created data generation script
    Refactor to use ArangoDB not MongoDB
    Setup newrelic

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
+require('newrelic');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-const db = require('../database/index.js');
-// const generateListing = require('../database/mockData.js');
+const findListing = require('../database/index.js');
 
 const PORT = process.env.PORT || 3002;
 const PUBLIC = path.resolve(__dirname, '..', 'client', 'dist');
@@ -15,49 +16,9 @@ app.use(express.static(PUBLIC));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// app.post('/listings/:id', function(req, res) {
-//   db.create(generateListing(), (err) => {
-//     if (err) {
-//       console.log(err);
-//       res.sendStatus(501);
-//     } else {
-//       res.sendStatus(201);
-//     }
-//   });
-// });
-
 app.get('/listings/:id', (req, res) => {
-  db.findOne({ id: req.params.id })
-    .exec((err, docs) => {
-      if (err) {
-        res.sendStatus(501);
-      } else {
-        res.status(200).send(docs);
-      }
-    });
+  findListing(req, res);
 });
-
-// app.put('/listings/:id', function(req, res) {
-//   db.update({id: 1}, {city: "Seattle"}, (err) => {
-//     if (err) {
-//       console.log(err);
-//       res.sendStatus(501);
-//     } else {
-//       res.sendStatus(201);
-//     }
-//   })
-// });
-
-// app.delete('/listings/:id', function(req, res) {
-//   db.deleteOne({numberOfBedrooms: 10}, (err) => {
-//     if(err) {
-//       console.log(err);
-//       res.sendStatus(501);
-//     } else {
-//       res.sendStatus(200);
-//     }
-//   })
-// });
 
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
